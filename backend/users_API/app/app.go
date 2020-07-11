a738b35d7aa76d6b71d38e62b1fb505d79dbb07c
parsa <parsaakbari1209@gmail.com>
2020-07-11 21:40:17 +0430

    Update main.go & create app pkg

---
+++ backend/users_API/app/app.go
@@ -0,0 +1,8 @@
+package app
+
+import "fmt"
+
+// StartApplication is called by main.go and starts the microservice.
+func StartApplication() {
+	fmt.Println("This is where uses microservice starts")
+}
---
+++ backend/users_API/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/group-work/backend/users_API/app"
+)
+
+func main() {
+	app.StartApplication()
+}
