aa144c358377c2f49e7bc5f52e9204d6baffd321
parsa <parsaakbari1209@gmail.com>
2020-07-11 21:25:40 +0430

    updating messages_API/main.go and creating app pkg

---
+++ backend/messages_API/app/app.go
@@ -0,0 +1,8 @@
+package app
+
+import "fmt"
+
+// StartApplication is called by the main func and starts the service.
+func StartApplication() {
+	fmt.Println("This is where messages microservice starts.")
+}
---
+++ backend/messages_API/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/group-work/backend/messages_API/app"
+)
+
+func main() {
+	app.StartApplication()
+}
