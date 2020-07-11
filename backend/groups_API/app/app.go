8fa15a3d61b0c3e4959b4c7b71d3d458e002f3b0
parsa <parsaakbari1209@gmail.com>
2020-07-11 21:32:48 +0430

    Update gropus_API/main.go & create app pkg

--- backend/groups_API/app/app.go
+++ backend/groups_API/app/app.go
@@ -4,5 +4,5 @@ import "fmt"
 
 // StartApplication is called by the main func and starts the microservice.
 func StartApplication() {
-	fmt.Println("This is where messages microservice starts.")
+	fmt.Println("This where users microservice starts.")
 }
---
+++ backend/groups_API/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/group-work/backend/groups_API/app"
+)
+
+func main() {
+	app.StartApplication()
+}
