80b48cb89a9ffae944016aebec98f7493bfb466b
parsa <parsaakbari1209@gmail.com>
2020-07-12 17:49:55 +0430

    Added more features to the User type.

--- backend/users_API/domain/dto.go
+++ backend/users_API/domain/dto.go
@@ -3,8 +3,9 @@ package domain
 // User type ...
 type User struct {
 	ID             int64  `json:"id"`
-	Name           string `json:"name"`
 	Email          string `json:"email"`
+	LastName       string `json:"last_name"`
+	FirstName      string `json:"first_name"`
 	DateCreated    string `json:"date_created"`
 	ProfilePicture string `json:"profile_picture"`
 }
