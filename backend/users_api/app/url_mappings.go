1ea5e4e9a14b489fe1c796403cf1ff510ddbd834
parsa <parsaakbari1209@gmail.com>
2020-07-15 17:28:42 +0430

    Added some router

--- backend/users_api/app/url_mappings.go
+++ backend/users_api/app/url_mappings.go
@@ -2,8 +2,11 @@ package app
 
 import (
 	"github.com/group-work/backend/users_api/controllers/ping"
+	"github.com/group-work/backend/users_api/controllers/users"
 )
 
 func mapURLs() {
 	router.GET("/ping", ping.Ping)
+
+	router.POST("/user/create", users.CreateUser)
 }
--- backend/users_api/controllers/users/users_controller.go
+++ backend/users_api/controllers/users/users_controller.go
@@ -1,1 +1,11 @@
 package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
