7e943473df67a0ff16a1e2246df8b72bdbf5d2ba
parsa <akbariparsa1209@gmail.com>
2020-08-08 17:43:09 +0430

    Deleted some enpoints.

--- backend/users_api/app/url_mappings.go
+++ backend/users_api/app/url_mappings.go
@@ -2,12 +2,8 @@ package app
 
 import (
 	"github.com/group-work/backend/users_api/controllers/ping"
-	"github.com/group-work/backend/users_api/controllers/users"
 )
 
 func mapURLs() {
 	router.GET("/ping", ping.Ping)
-
-	router.GET("/users/get/:user_id", users.GetUser)
-	router.POST("/users/create", users.CreateUser)
 }
