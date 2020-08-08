0debeb4ab5f79854b4fa323758a948019e34d163
parsa <akbariparsa1209@gmail.com>
2020-08-08 17:58:18 +0430

    Added rest error type and funcs.

--- backend/users_api/utils/errors/error.go
+++ backend/users_api/utils/errors/error.go
@@ -1,1 +1,31 @@
 package errors
+
+type RestErr struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
+}
+
+func BadRequest(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  400,
+		Error:   "bad request",
+	}
+}
+
+func NotFound(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  404,
+		Error:   "not found",
+	}
+}
+
+func InternalServerErr(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  500,
+		Error:   "internal server error",
+	}
+}
