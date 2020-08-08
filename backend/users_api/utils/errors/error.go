7bdcf615851832ccda67a37dd4e18ab4028ed1a5
parsa <akbariparsa1209@gmail.com>
2020-08-08 17:45:29 +0430

    Edited and deleted some utils.

--- backend/users_api/utils/errors/error.go
+++ backend/users_api/utils/errors/error.go
@@ -1,16 +1,1 @@
 package errors
-
-type RESTError struct {
-	Message string
-	Status  int
-	Error   string
-}
-
-func BadRequestError(message string) *RESTError {
-	err := RESTError{
-		Message: message,
-		Status:  400,
-		Error:   "Bad_Request",
-	}
-	return &err
-}
